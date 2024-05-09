0b1631b5039389d0c707c19d1fa7d8a45e750fbf
Sam10907 <42615337+Sam10907@users.noreply.github.com>
2024-05-09 23:03:02 +0800

    implement virtual memory

--- src/main.c
+++ src/main.c
@@ -4,11 +4,11 @@
 
 void boot_main(){
 	uart_printf("\n\n _  _  ___ _____ _   _  ___  ___ ___ ___ \n");
-    uart_printf("| \\| |/ __|_   _| | | |/ _ \\/ __|   \\_ _|\n");
-    uart_printf("| .` | (__  | | | |_| | (_) \\__ \\ |) | | \n");
-    uart_printf("|_|\\_|\\___| |_|  \\___/ \\___/|___/___/___|\n\n");
-
+	uart_printf("| \\| |/ __|_   _| | | |/ _ \\/ __|   \\_ _|\n");
+	uart_printf("| .` | (__  | | | |_| | (_) \\__ \\ |) | | \n");
+	uart_printf("|_|\\_|\\___| |_|  \\___/ \\___/|___/___/___|\n\n");
+	
 	mm_init();
-    task_init();
-    schedule_init();
+	task_init();
+	schedule_init();
 }
